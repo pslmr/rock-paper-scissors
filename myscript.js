dd3e39129e2ea23530cc9ec7e9695b5854559455
Psalmer Abalon <abalonpsalmer@gmail.com>
2022-07-24 22:07:33 +0800

    Add playerSelect prompt to myscript.js

--- myscript.js
+++ myscript.js
@@ -4,3 +4,6 @@ function getComputerChoice() {
   const compChoice = Math.floor(Math.random() * 3) + 1;
   return compChoice === 1 ? "rock" : compChoice === 2 ? "paper" : "scissors";
 }
+
+const playerSelect = prompt('Enter your play:  "Rock,paper or scissors" ', "");
+const computerSelect = getComputerChoice();
